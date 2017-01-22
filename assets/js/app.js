54502e687c6915ec7965a2e6a8a92bd088dda4c1
StyvekDrtic <jiristepanek@hotmail.com>
2017-01-22 15:47:11 +0100

    galerie

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,3 +27,42 @@ function cela() {
 
 setTimeout(cela, 0);
 setInterval(cela, 21000);
+
+// DALSI
+
+var obrazky = [["JOJO tohle je popisek", "../img/obrazky/test.png"], ["dalsi", "../img/obrazky/test.png"]];
+var cislo = 0;
+
+$(".slider span:last-child").click(function(){
+  $(".slider").css("display", "none");
+});
+
+$(".Gobrazek").click(function(){
+  var element = $(this);
+  cislo = parseInt(element.children().children().attr("alt"));
+
+  $(".lista p").text(obrazky[cislo][0]);
+  $(".slider").css("display", "block");
+});
+
+$(".lista a:first-child").click(function(){
+  cislo--;
+
+  if (cislo < 0) {
+    cislo = 0;
+  }
+
+  $(".lista p").text(obrazky[cislo][0]);
+});
+
+$(".lista a:last-child").click(function(){
+  cislo++;
+
+  if (cislo >= obrazky.length) {
+    cislo = obrazky.length - 1;
+  }
+
+  $(".lista p").text(obrazky[cislo][0]);
+
+
+});
