1dd2c6f8241c28d17e270a4d2f50107784bbc942
StyvekDrtic <jiristepanek@hotmail.com>
2017-01-30 17:29:20 +0100

    foto do galerie

--- assets/js/app.js
+++ assets/js/app.js
@@ -30,7 +30,24 @@ setInterval(cela, 21000);
 
 // DALSI
 
-var obrazky = [["JOJO tohle je popisek", "assets/img/obrazky/test.png"], ["dalsi", "assets/img/obrazky/test.png"]];
+var obrazky = [
+              ["", "assets/img/obrazky/IMG_1.JPG"], ["", "assets/img/obrazky/IMG_2.JPG"], ["", "assets/img/obrazky/IMG_3.JPG"],
+              ["", "assets/img/obrazky/IMG_4.JPG"], ["", "assets/img/obrazky/IMG_5.JPG"], ["", "assets/img/obrazky/IMG_6.JPG"],
+              ["", "assets/img/obrazky/IMG_7.JPG"], ["", "assets/img/obrazky/IMG_8.JPG"], ["", "assets/img/obrazky/IMG_9.JPG"],
+
+              ["", "assets/img/obrazky/IMG_4420.JPG"], ["", "assets/img/obrazky/IMG_4423.JPG"], ["", "assets/img/obrazky/IMG_4428.JPG"],
+              ["", "assets/img/obrazky/IMG_4430.JPG"], ["", "assets/img/obrazky/IMG_4919.JPG"], ["", "assets/img/obrazky/IMG_4921.JPG"],
+              ["", "assets/img/obrazky/IMG_4922.JPG"], ["", "assets/img/obrazky/IMG_4923.JPG"], ["", "assets/img/obrazky/IMG_4924.JPG"],
+              ["", "assets/img/obrazky/IMG_4926.JPG"], ["", "assets/img/obrazky/IMG_5498.JPG"], ["", "assets/img/obrazky/IMG_5727.JPG"],
+              ["", "assets/img/obrazky/IMG_5728.JPG"], ["", "assets/img/obrazky/IMG_5736.JPG"], ["", "assets/img/obrazky/IMG_5741.JPG"],
+              ["", "assets/img/obrazky/IMG_5745.JPG"], ["", "assets/img/obrazky/IMG_5746.JPG"], ["", "assets/img/obrazky/IMG_5749.JPG"],
+              ["", "assets/img/obrazky/IMG_5753.JPG"], ["", "assets/img/obrazky/IMG_5760.JPG"], ["", "assets/img/obrazky/IMG_5761.JPG"],
+              ["", "assets/img/obrazky/IMG_5763.JPG"], ["", "assets/img/obrazky/IMG_5764.JPG"], ["", "assets/img/obrazky/IMG_5765.JPG"],
+              ["", "assets/img/obrazky/IMG_5766.JPG"], ["", "assets/img/obrazky/IMG_5769.JPG"], ["", "assets/img/obrazky/IMG_5771.JPG"],
+              ["", "assets/img/obrazky/IMG_5772.JPG"], ["", "assets/img/obrazky/IMG_5776.JPG"], ["", "assets/img/obrazky/IMG_5779.JPG"],
+              ["", "assets/img/obrazky/IMG_5858.JPG"], ["", "assets/img/obrazky/IMG_5922.JPG"], ["", "assets/img/obrazky/IMG_5981.JPG"],
+              ["", "assets/img/obrazky/IMG_5985.JPG"], ["", "assets/img/obrazky/IMG_5987.JPG"], ["", "assets/img/obrazky/IMG_5990.JPG"],
+              ["", "assets/img/obrazky/IMG_6007.JPG"]];
 var cislo = 0;
 
 $(".slider span:last-child").click(function(){
